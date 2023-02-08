b132a29c0aa4422e24d9a6336c4b7711b0656a01
RyanNg <>
2023-02-08 17:01:20 +0800

    First commit

---
+++ src/main/java/DCOMS_Assignment.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author ryann
+ */
+public class DCOMS_Assignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
