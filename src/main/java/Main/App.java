3b7ba34c5f2d17255e8521d5b83b0174039ef60b
BlueSky0322 <ryanng8628@gmail.com>
2023-02-13 08:47:20 +0800

    Changed class name "DCOMS_Assignment" to "App"

---
+++ src/main/java/Main/App.java
@@ -0,0 +1,17 @@
+package Main;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author ryann
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //Testing GitHub
+    }
+}
