6fdd10c2ac0682ffada00925b403e51e57f39b48
BlueSky0322 <ryanng8628@gmail.com>
2023-02-13 08:44:35 +0800

    Register Class

---
+++ src/main/java/RMIConnections/Register.java
@@ -0,0 +1,27 @@
+package RMIConnections;
+
+
+import java.rmi.*;
+import java.rmi.registry.*;
+
+//solely used to register port number, essentially creating a socket connection between server and client
+public class Register {
+
+    private final int portNumber;
+
+    public Register() {
+        this.portNumber = 6969;
+    }
+
+    public static void main(String[] args) throws RemoteException {
+        Register Reg = new Register();
+        try {
+            Registry socketclient = LocateRegistry.createRegistry(Reg.portNumber);
+            System.out.println("Port registered at: " + Reg.portNumber);
+            socketclient.rebind("DCOMS", new Server());
+            System.out.println("Server is running...");
+        } catch (RemoteException ex) {
+            System.out.println(ex);
+        }
+    }
+}
