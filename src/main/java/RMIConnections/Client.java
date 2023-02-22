c2d23ab22c430a9f85afadd9c829907accce0f11
BlueSky0322 <ryanng8628@gmail.com>
2023-02-22 15:53:44 +0800

    Merge from master/Han

--- src/main/java/RMIConnections/Client.java
+++ src/main/java/RMIConnections/Client.java
@@ -5,8 +5,9 @@ import java.net.*;
 import java.rmi.*;
 
 public class Client {
-    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
-        Interface Object = (Interface) Naming.lookup("rmi://localhost:6969/DCOMS");
+    public static Interface Object;
+    public Client() throws RemoteException, NotBoundException, MalformedURLException {
+        Object = (Interface) Naming.lookup("rmi://localhost:6969/DCOMS");
     }
 }
 
