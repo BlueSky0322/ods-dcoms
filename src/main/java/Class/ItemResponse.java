aeeb808e58a5ab4fbb1d511984bd7c39d2c3e43e
BlueSky0322 <ryanng8628@gmail.com>
2023-02-23 16:56:34 +0800

    Modified properties

--- src/main/java/Class/ItemResponse.java
+++ src/main/java/Class/ItemResponse.java
@@ -13,15 +13,11 @@ import static Class.utils.GUID.Generate;
 public class ItemResponse extends Item {
 
     private String RequestID;
-    private int Code;
-    private int ErrorType;
     private String ErrorDetail;
 
-    public ItemResponse(String name, double unitPrice, int stockAmount, String RequestID, int Code, int ErrorType, String ErrorDetail) {
+    public ItemResponse(String name, double unitPrice, int stockAmount, String RequestID, String ErrorDetail) {
         super(name, unitPrice, stockAmount);
         this.RequestID = Generate().toString();
-        this.Code = Code;
-        this.ErrorType = ErrorType;
         this.ErrorDetail = ErrorDetail;
     }
 }
