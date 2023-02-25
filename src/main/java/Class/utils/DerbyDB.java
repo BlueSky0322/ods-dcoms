d89a3eb2d5d99934b4039653a9fb7cb25cee37ab
BlueSky0322 <ryanng8628@gmail.com>
2023-02-25 17:59:53 +0800

    Added initialiseCustomerTable, initialiseItemTable

--- src/main/java/Class/utils/DerbyDB.java
+++ src/main/java/Class/utils/DerbyDB.java
@@ -7,32 +7,95 @@ package Class.utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author handikaharianto
  */
 public class DerbyDB {
+
     private final static String DATABASE_URL = "jdbc:derby://localhost:1527/ODS";
+    private final static String DATABASE_USERNAME = "ods";
+    private final static String DATABASE_PASSWORD = "ods";
     private static Connection dbConnection;
-    
+
     public static void connectDB() {
         try {
-            dbConnection = DriverManager.getConnection(DATABASE_URL);
+            dbConnection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
             dbConnection.setAutoCommit(false);
+            System.out.println("Database is running...");
+            System.out.println("Checking tables...");
+            //initialise all tables
+            initialiseItemTable(createStatement());
+            initialiseCustomerTable(createStatement());
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
     }
-    
+
     public static Statement createStatement() throws SQLException {
         return dbConnection.createStatement();
     }
-    
+
     public static PreparedStatement preparedStatement(String query) throws SQLException {
         return dbConnection.prepareStatement(query);
+    }
+
+    public static void commit() throws SQLException {
+        dbConnection.commit();
+    }
+
+    public static void initialiseItemTable(Statement stmt) throws SQLException {
+        String createItemTableQuery;
+        ResultSet rs;
+        try {
+            rs = dbConnection.getMetaData().getTables(null, null, "ITEM", null);
+            if (!rs.next()) {
+                createItemTableQuery
+                        = "CREATE TABLE Item ("
+                        + "item_id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"
+                        + "item_name VARCHAR(255) NOT NULL,"
+                        + "unit_price DECIMAL(10,2) NOT NULL,"
+                        + "stock_amount INT NOT NULL)";
+                stmt.executeUpdate(createItemTableQuery);
+                System.out.println("Table 'Item' created successfully.");
+                commit();
+            } else {
+                System.out.println("Table 'Item' already exists.");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error creating/checking table: " + ex.getMessage());
+        }
+    }
+
+    public static void initialiseCustomerTable(Statement stmt) {
+        String createCustomerTableQuery;
+        ResultSet rs;
+        try {
+            rs = dbConnection.getMetaData().getTables(null, null, "CUSTOMER", null);
+            if (!rs.next()) {
+                createCustomerTableQuery
+                        = "CREATE TABLE Customer ("
+                        + "cust_id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"
+                        + "username VARCHAR(15) NOT NULL UNIQUE,"
+                        + "password VARCHAR(255) NOT NULL,"
+                        + "first_name VARCHAR(45) NOT NULL,"
+                        + "last_name VARCHAR(45) NOT NULL,"
+                        + "passport VARCHAR(9) NOT NULL UNIQUE)";
+                stmt.executeUpdate(createCustomerTableQuery);
+                System.out.println("Table 'Customer' created successfully.");
+                commit();
+            } else {
+                System.out.println("Table 'Customer' already exists.");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error creating/checking table: " + ex.getMessage());
+        }
     }
 }
