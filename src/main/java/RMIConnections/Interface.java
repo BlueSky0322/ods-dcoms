da53c4e0c917046ff71090f427e8ed3b1b1d1d88
BlueSky0322 <ryanng8628@gmail.com>
2023-02-27 01:53:40 +0800

    feat: displayTable() interface

--- src/main/java/RMIConnections/Interface.java
+++ src/main/java/RMIConnections/Interface.java
@@ -3,6 +3,9 @@ package RMIConnections;
 import Class.User;
 import Class.Item;
 import java.rmi.*;
+import java.sql.SQLException;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -13,4 +16,5 @@ public interface Interface extends Remote{
     public User login(User user) throws Exception;
     public void register(User newUser) throws Exception;
     public void addItem(Item newItem) throws Exception;
+    public DefaultTableModel displayTable() throws Exception;
 }
