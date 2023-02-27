18dcb410fa67c9f7db2accf2f7c3f7fc94a521ea
BlueSky0322 <ryanng8628@gmail.com>
2023-02-28 06:33:03 +0800

    feat: all admin CRUD server-side functionalities interfaces

--- src/main/java/RMIConnections/Interface.java
+++ src/main/java/RMIConnections/Interface.java
@@ -4,6 +4,7 @@ import Class.User;
 import Class.Item;
 import java.rmi.*;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
@@ -15,6 +16,11 @@ public interface Interface extends Remote{
     public void placeholderMethod() throws RemoteException;
     public User login(User user) throws Exception;
     public void register(User newUser) throws Exception;
+    
     public void addItem(Item newItem) throws Exception;
-    public DefaultTableModel displayTable() throws Exception;
+    public void updateItem(int itemId, Item updatedItem) throws Exception;
+    public void deleteItem(Item currentItem) throws Exception;
+    public DefaultTableModel viewTable() throws Exception;
+    public Item retrieveItemByID(String itemID) throws Exception;
+    public ArrayList<String> retrieveAllItemID() throws Exception;
 }
